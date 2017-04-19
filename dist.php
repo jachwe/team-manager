ed5e6671972fc8e4531f894aca15146c39d3ea16
Jewe Pasch <jewe@sedna.de>
2017-04-19 15:00:10 +0200

    fixed js dist files

--- dist.php
+++ dist.php
@@ -19,15 +19,17 @@ $minifier->minify('public/css/dist.css');
 $minifier = new Minify\JS();
 $minifier->add('public/js/jquery.js');
 $minifier->add('public/js/bootstrap.min.js');
+$minifier->add('public/js/bootstrap-multiselect.js');
 $minifier->add('public/js/bootstrap-datetimepicker.min.js');
 $minifier->add('public/js/bootstrap-typeahead.js');
-$minifier->add('public/js/bootstrap-multiselect.js');
 $minifier->add('public/js/list.js');
 $minifier->add('public/summernote/summernote.min.js');
 $minifier->add('public/summernote/lang/summernote-de-DE.js');
+
 $minifier->add('public/js/main.js');
 
-$minifier->minify('public/js/dist.js');
+
+echo $minifier->minify('public/js/dist.js');
 
 // save minified file to disk
 // $minifiedPath = '/path/to/minified/css/file.css';
